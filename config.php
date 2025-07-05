8937945a24a705512874c2075c95250a627c36e6
juan-gil-05 <gilj06@hotmail.com>
2025-07-05 23:50:57 +0200

    fix: Refactor environment variable loading to check for .env file existence

--- config.php
+++ config.php
@@ -4,6 +4,26 @@ require_once BASE_PATH . '/vendor/autoload.php';
 // File to initilaize env variables with the vlucas/phpdotenv library
 use Dotenv\Dotenv;
 
-$dotenv = Dotenv::createImmutable(BASE_PATH);
-$dotenv->load();
-return $_ENV;
+
+if (file_exists(BASE_PATH . '/.env')) {
+    // To charge .env if exists (in local)
+    $dotenv = Dotenv::createImmutable(BASE_PATH);
+    $dotenv->load();
+    return $_ENV;
+}
+// else, use the getenv 
+return [
+    // Mysql
+    'MYSQL_DATABASE' => getenv('MYSQL_DATABASE'),
+    'MYSQL_USER' => getenv('MYSQL_USER'),
+    'MYSQL_PASSWORD' => getenv('MYSQL_PASSWORD'),
+    'MYSQL_PORT' => getenv('MYSQL_PORT'),
+    'MYSQL_HOST' => getenv('MYSQL_HOST'),
+    // MongoDB
+    'MONGO_INITDB_ROOT_USERNAME' => getenv('MONGO_INITDB_ROOT_USERNAME'),
+    'MONGO_INITDB_ROOT_PASSWORD' => getenv('MONGO_INITDB_ROOT_PASSWORD'),
+    'MONGO_PORT' => getenv('MONGO_PORT'),
+    'MYSQL_HOST' => getenv('MONGO_HOST'),
+    // Encrypter Key
+    'ENCRYPTER_KEY' => getenv('ENCRYPTER_KEY')
+];
